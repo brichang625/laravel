fa74c569f6704b07a4ac3a9169cabd6fa4e12a3d
Briana Chang <macbookpro@MacBook-Pro-de-MacBook-4.local>
2026-09-24 17:43:28 -0500

    Initial Laravel project

---
+++ app/Models/Persona.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nombre', 'email'];
+    public function intereses()
+    {
+        return $this->belongsToMany(Interes::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\InteresController;
+use App\Http\Controllers\UserController;
+
+// Protected routes by authentication
+Route::middleware(['auth'])->group(function () {
+    Route::resource('personas', PersonaController::class);
+    Route::resource('intereses', InteresController::class);
+    Route::get('/usuarios', [UserController::class, 'index'])
+        ->name('usuarios.index');
+});
